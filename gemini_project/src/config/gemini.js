a846b393afc64080f92809b23cef4e6e9eeb60fa
Adib-96 <adibbh1996@gmail.com>
2024-06-13 17:49:56 +0100

    App based on gemini API

---
+++ gemini_project/src/config/gemini.js
@@ -0,0 +1,47 @@
+
+/*
+ * Install the Generative AI SDK
+ *
+ * $ npm install @google/generative-ai
+ *
+ * See the getting started guide for more information
+ * https://ai.google.dev/gemini-api/docs/get-started/node
+ */
+
+import {
+  GoogleGenerativeAI,
+  HarmCategory,
+  HarmBlockThreshold,
+}  from "@google/generative-ai";
+
+const apiKey = 'AIzaSyDgO4bOhB5dDLcsVyvLhMv0sk8_PA12hN8';
+const genAI = new GoogleGenerativeAI(apiKey);
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-flash",
+});
+
+const generationConfig = {
+  temperature: 1,
+  topP: 0.95,
+  topK: 64,
+  maxOutputTokens: 8192,
+  responseMimeType: "text/plain",
+};
+
+async function run(prompt) {
+  const chatSession = model.startChat({
+    generationConfig,
+ // safetySettings: Adjust safety settings
+ // See https://ai.google.dev/gemini-api/docs/safety-settings
+    history: [
+    ],
+  });
+
+  const result = await chatSession.sendMessage(prompt);
+  console.log(result.response.text());
+  const response = result.response;
+  return response.text();
+}
+
+export default run;
